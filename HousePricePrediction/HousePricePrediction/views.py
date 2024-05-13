a4ea88f5ad18909fa1795148c5c203e7c41dc459
GAU-DEPTRAI\Admin <chienho10052003@gmail.con>
2024-05-13 19:07:33 +0700

    edit: dont used to sklearn libarary

--- HousePricePrediction/HousePricePrediction/views.py
+++ HousePricePrediction/HousePricePrediction/views.py
@@ -1,8 +1,5 @@
 import pandas as pd
-import numpy as np
 from django.shortcuts import render
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
 
 def home(request):
     return render(request, 'home.html')
@@ -14,13 +11,20 @@ def result(request):
     data = pd.read_csv('USA_housing.csv')
     data = data.drop('Address', axis=1)
     
-    X = data.drop('Price', axis = 1)
+    X = data.drop('Price', axis=1)
     Y = data['Price']
 
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.30)
+    # Phân chia dữ liệu thành tập huấn luyện và tập kiểm tra (30% cho kiểm tra)
+    train_size = int(0.7 * len(X))
+    X_train, X_test = X[:train_size], X[train_size:]
+    Y_train, Y_test = Y[:train_size], Y[train_size:]
     
-    model = LinearRegression()
-    model.fit(X_train, Y_train)
+    # Tính toán các giá trị trung bình của các thuộc tính
+    avg_values = X_train.mean(axis=0)
+    
+    # Hàm dự đoán giá nhà dựa trên các giá trị trung bình của thuộc tính
+    def predict_price(var1, var2, var3, var4, var5):
+        return avg_values[0] * var1 + avg_values[1] * var2 + avg_values[2] * var3 + avg_values[3] * var4 + avg_values[4] * var5
     
     var1 = float(request.GET['n1'])
     var2 = float(request.GET['n2'])
@@ -28,8 +32,8 @@ def result(request):
     var4 = float(request.GET['n4'])
     var5 = float(request.GET['n5'])
     
-    pred = model.predict(np.array([var1, var2, var3, var4, var5]).reshape(1,-1))
-    pred = round(pred[0])
+    pred = predict_price(var1, var2, var3, var4, var5)
+    pred = round(pred)
     
-    price = "The predicted price is: $"+str(pred)
+    price = "The predicted price is: $" + str(pred)
     return render(request, 'predict.html', {"result2": price})
